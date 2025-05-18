9ff45b7c439b369c1b67de42ab68f4d32a39abbe
mgiks <lidamil6785@gmail.com>
2025-05-18 22:45:57 +0600

    fix: make redis client actually connect to the database

--- backend/internal/db/redis/redis.go
+++ backend/internal/db/redis/redis.go
@@ -2,35 +2,23 @@ package redis
 
 import (
 	"context"
-	"fmt"
-	"log"
 
-	"github.com/mgiks/ttyper/internal/utils"
 	"github.com/redis/go-redis/v9"
 )
 
 type Database struct {
-	client *redis.Client
+	Client *redis.Client
 }
 
 func New(ctx context.Context) *Database {
-	opt, err := redis.ParseURL(getDBURL())
-	if err != nil {
-		log.Fatalln("Unable to connect to redis database:", err)
+	return &Database{
+		Client: redis.NewClient(
+			&redis.Options{
+				Addr:     "localhost:6379",
+				Password: "",
+				DB:       0,
+				Protocol: 2,
+			},
+		),
 	}
-
-	return &Database{client: redis.NewClient(opt)}
-}
-
-func getDBURL() string {
-	dbPass := utils.FindEnvOrFail("REDIS_PASSWORD")
-	dbPort := utils.FindEnvOrFail("REDIS_PORT")
-	dbName := utils.FindEnvOrFail("REDIS_DB")
-
-	return fmt.Sprintf(
-		"redis://redis:%s@localhost:%s/%s",
-		dbPass,
-		dbPort,
-		dbName,
-	)
 }
