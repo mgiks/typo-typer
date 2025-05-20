7667ca83d4a368aca50156816e62a23390888500
mgiks <lidamil6785@gmail.com>
2025-05-20 20:22:05 +0600

    test: remove unnecessary fixture and give a better name to an existing one

--- frontend/src/components/typing-stats-container/utils/tests/calculateWordCount.test.tsx
+++ frontend/src/components/typing-stats-container/utils/tests/calculateWordCount.test.tsx
@@ -1,8 +1,7 @@
 import {
-  emptyText,
   emptyTextWithWhitespaces,
   text,
-  textWithVaryingWhitespaceLength,
+  textWithIrregularSpacing,
 } from './fixtures'
 import { calculateWordCount } from '../calculateWordCount'
 
@@ -10,9 +9,9 @@ describe('calculateWordCount', () => {
   it('should count 8 words from regular text', () =>
     expect(calculateWordCount(text)).toBe(8))
   it('should count 0 words from empty text', () =>
-    expect(calculateWordCount(emptyText)).toBe(0))
+    expect(calculateWordCount('')).toBe(0))
   it('should count 0 words from text with spaces only', () =>
     expect(calculateWordCount(emptyTextWithWhitespaces)).toBe(0))
-  it('should count 8 words from text with varying space length', () =>
-    expect(calculateWordCount(textWithVaryingWhitespaceLength)).toBe(8))
+  it('should count 8 words from text with irregular spacing', () =>
+    expect(calculateWordCount(textWithIrregularSpacing)).toBe(8))
 })
--- frontend/src/components/typing-stats-container/utils/tests/fixtures.tsx
+++ frontend/src/components/typing-stats-container/utils/tests/fixtures.tsx
@@ -1,5 +1,4 @@
 export const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-export const emptyText = ''
 export const emptyTextWithWhitespaces = '       \t'
-export const textWithVaryingWhitespaceLength =
+export const textWithIrregularSpacing =
   'Lorem     ipsum  dolor     \tsit  amet,  consectetur   adipiscing    elit.'
