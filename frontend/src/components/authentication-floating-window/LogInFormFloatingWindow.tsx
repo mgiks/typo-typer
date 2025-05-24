0849b061af855eee5ad0e6ee5d9ad2380ff40274
mgiks <lidamil6785@gmail.com>
2025-05-24 18:52:10 +0600

    feat: add button to submit log-in form

--- frontend/src/components/authentication-floating-window/LogInFormFloatingWindow.tsx
+++ frontend/src/components/authentication-floating-window/LogInFormFloatingWindow.tsx
@@ -1,12 +1,26 @@
+import { useRef } from 'react'
 import './LogInFormFloatingWindow.css'
 
 function LogInFormFloatingWindow(
   { shouldLogInFormBeShown }: { shouldLogInFormBeShown: boolean },
 ) {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  function submitForm() {
+    if (!formRef.current) return
+
+    const data = new FormData(formRef.current)
+
+    fetch('http://localhost:8000/users', {
+      method: 'post',
+      body: data,
+    })
+  }
+
   const logInForm = (
     <div id='log-in-form-wrapper'>
       Log in:
-      <form id='log-in-form'>
+      <form ref={formRef} id='log-in-form' onSubmit={submitForm}>
         <label htmlFor='name'>Name:</label>
         <input type='text' id='name' />
 
@@ -15,6 +29,8 @@ function LogInFormFloatingWindow(
 
         <label htmlFor='password'>Password:</label>
         <input type='password' id='password' />
+
+        <input type='submit' value={'Log In'} />
       </form>
     </div>
   )
