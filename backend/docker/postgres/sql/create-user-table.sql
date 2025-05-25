1139808fa8bdc6d23c82bbcdb0e63d09169590c1
mgiks <lidamil6785@gmail.com>
2025-05-25 23:55:56 +0600

    feat: add initiazation sql queries for 'user' table'

---
+++ backend/docker/postgres/sql/create-user-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "user" (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name varchar(128) NOT NULL,
+    email varchar(320) NOT NULL,
+    hashed_password varchar(256) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
