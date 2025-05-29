92717b1b90e52d7808ba1316e4f72c2d91bac49a
mgiks <lidamil6785@gmail.com>
2025-05-30 00:56:16 +0600

    refactor: move common error checking into a separate function in 'jwt_test'

--- backend/internal/auth/jwt_test.go
+++ backend/internal/auth/jwt_test.go
@@ -10,7 +10,6 @@ import (
 
 func TestGenerateJWT(t *testing.T) {
 	seed := make([]byte, ed25519.SeedSize)
-
 	data := jwt.MapClaims{
 		"user":         "mgik",
 		"isADeveloper": true,
@@ -18,10 +17,7 @@ func TestGenerateJWT(t *testing.T) {
 
 	t.Run("should consist of 3 parts separated by dots", func(t *testing.T) {
 		token, err := GenerateJWT(seed, data)
-
-		if err != nil {
-			t.Errorf("failed to generate jwt: %v", err)
-		}
+		assertNoErrors(t, err)
 
 		parts := bytes.Split([]byte(token), []byte("."))
 
@@ -33,13 +29,20 @@ func TestGenerateJWT(t *testing.T) {
 	t.Run("should be idempotent", func(t *testing.T) {
 		token, err := GenerateJWT(seed, data)
 		token2, err2 := GenerateJWT(seed, data)
-
-		if err != nil || err2 != nil {
-			t.Errorf("failed to generate jwt: %v", err)
-		}
+		assertNoErrors(t, err, err2)
 
 		if token != token2 {
 			t.Errorf("the first result %v differs from the second result %v", token, token2)
 		}
 	})
+}
+
+func assertNoErrors(t testing.TB, errs ...error) {
+	t.Helper()
+
+	for _, err := range errs {
+		if err != nil {
+			t.Errorf("failed to generate jwt: %v", err)
+		}
+	}
 }
