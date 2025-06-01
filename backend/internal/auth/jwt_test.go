abb1f5eef8d54ae5b7b7f2ac1821d1512185c918
mgiks <lidamil6785@gmail.com>
2025-06-01 12:55:39 +0600

    refactor: move some code into its own functions in 'TestGenerateJWT' function

--- backend/internal/auth/jwt_test.go
+++ backend/internal/auth/jwt_test.go
@@ -1,8 +1,8 @@
 package auth
 
 import (
-	"bytes"
 	"crypto/ed25519"
+	"strings"
 	"testing"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -10,36 +10,40 @@ import (
 
 func TestGenerateJWT(t *testing.T) {
 	seed := make([]byte, ed25519.SeedSize)
-	data := jwt.MapClaims{
-		"user":         "mgik",
-		"isADeveloper": true,
-	}
+	data := jwt.MapClaims{"user": "mgik"}
 
 	t.Run("should consist of 3 parts separated by dots", func(t *testing.T) {
 		token, err := GenerateJWT(seed, data)
+
 		assertNoErrors(t, err)
-
-		parts := bytes.Split([]byte(token), []byte("."))
-
-		if len(parts) != 3 {
-			t.Errorf("token doesn't consist of 3 parts: %v", token)
-		}
+		assertConsistsOf3Parts(t, token)
 	})
 
 	t.Run("should be idempotent", func(t *testing.T) {
-		token, err := GenerateJWT(seed, data)
+		token1, err := GenerateJWT(seed, data)
 		token2, err2 := GenerateJWT(seed, data)
-		assertNoErrors(t, err, err2)
 
-		if token != token2 {
-			t.Errorf("the first result %v differs from the second result %v", token, token2)
-		}
+		assertNoErrors(t, err, err2)
+		assertTokensAreEqual(t, token1, token2)
 	})
+}
+
+func assertTokensAreEqual(t testing.TB, token1, token2 string) {
+	t.Helper()
+	if token1 != token2 {
+		t.Errorf("%q is not equal to %q", token1, token2)
+	}
+}
+
+func assertConsistsOf3Parts(t testing.TB, token string) {
+	t.Helper()
+	if len(strings.Split(token, ".")) != 3 {
+		t.Errorf("token doesn't consist of 3 parts: %v", token)
+	}
 }
 
 func assertNoErrors(t testing.TB, errs ...error) {
 	t.Helper()
-
 	for _, err := range errs {
 		if err != nil {
 			t.Errorf("failed to generate jwt: %v", err)
