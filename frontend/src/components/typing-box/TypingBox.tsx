ac8fd9b8f6ad331b330f47268c7a0f2d650dd4b1
mgiks <lidamil6785@gmail.com>
2025-07-10 23:10:59 +0600

    feat: make 'TypingBox' fetch text

--- frontend/src/components/typing-box/TypingBox.tsx
+++ frontend/src/components/typing-box/TypingBox.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
 import './TypingBox.scss'
 import InputCatcher from './input-catcher/InputCatcher'
 
+const TEXTS_URL = 'http://localhost:8000/texts'
+
+export type GETTextResponse = {
+  text: string
+}
+
 function TypingBox() {
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    try {
+      fetch(TEXTS_URL)
+        .then((resp) => resp.json())
+        .then((resp) => resp as GETTextResponse)
+        .then((json) => setText(json.text))
+    } catch (e) {
+    }
+  })
+
   return (
     <div className='typing-box' data-testid='typing-box'>
       <InputCatcher />
+      {text}
     </div>
   )
 }
