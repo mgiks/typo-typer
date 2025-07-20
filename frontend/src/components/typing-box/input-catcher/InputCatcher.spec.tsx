8e4b7fe64a3ba2f48dc08cea48c08dc2d8f9b7eb
mgiks <lidamil6785@gmail.com>
2025-07-20 22:29:34 +0600

    test(InputCatcher): add test for being in the document

--- frontend/src/components/typing-box/input-catcher/InputCatcher.spec.tsx
+++ frontend/src/components/typing-box/input-catcher/InputCatcher.spec.tsx
@@ -2,6 +2,12 @@ import { render, screen } from '@testing-library/react'
 import InputCatcher from './InputCatcher.tsx'
 
 describe('InputCatcher', async () => {
+  it('should be in the document', async () => {
+    render(<InputCatcher />)
+
+    expect(await screen.findByTestId('input-catcher')).toBeInTheDocument()
+  })
+
   it('should be initially focused', async () => {
     render(<InputCatcher />)
 
