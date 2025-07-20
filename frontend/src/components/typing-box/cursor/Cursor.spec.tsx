10c79bed595f39ea15191464bcb033648eb95230
mgiks <lidamil6785@gmail.com>
2025-07-20 22:24:04 +0600

    chore: fix a wrong test description

--- frontend/src/components/typing-box/cursor/Cursor.spec.tsx
+++ frontend/src/components/typing-box/cursor/Cursor.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Cursor from './Cursor.tsx'
 
-describe('InputCatcher', async () => {
+describe('Cursor', async () => {
   it('should be in the document', async () => {
     render(<Cursor />)
 
