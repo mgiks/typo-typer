a4d01c441592c59c52e8f14216aa19b1e854e741
mgiks <lidamil6785@gmail.com>
2025-08-01 10:53:29 +0600

    feat(ReminderToFocus): hide on click

--- frontend/src/components/typing-box/reminder-to-focus/ReminderToFocus.tsx
+++ frontend/src/components/typing-box/reminder-to-focus/ReminderToFocus.tsx
@@ -2,17 +2,22 @@ import './ReminderToFocus.scss'
 
 type ReminderToFocusProps = {
   focused: boolean
+  focusSetter: (i: boolean) => void
 }
 
-function ReminderToFocus({ focused }: ReminderToFocusProps) {
-  if (focused) {
-    return null
-  }
-
-  return (
-    <div className='reminder-to-focus' data-testid='reminder-to-focus'>
+function ReminderToFocus({ focused, focusSetter }: ReminderToFocusProps) {
+  const reminderToFocus = (
+    <div
+      className='reminder-to-focus'
+      data-testid='reminder-to-focus'
+      onClick={() => focusSetter(true)}
+    >
       Click here or press any key to focus
     </div>
+  )
+
+  return (
+    focused ? null : reminderToFocus
   )
 }
 
