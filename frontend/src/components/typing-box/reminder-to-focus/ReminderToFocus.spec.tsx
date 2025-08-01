533a2cc3e71608073cc69af4cf87b5acf1369af7
mgiks <lidamil6785@gmail.com>
2025-08-01 20:53:49 +0600

    feat(ReminderToFocus): focus ref passed as a prop

--- frontend/src/components/typing-box/reminder-to-focus/ReminderToFocus.spec.tsx
+++ frontend/src/components/typing-box/reminder-to-focus/ReminderToFocus.spec.tsx
@@ -5,13 +5,33 @@ import { useState } from 'react'
 
 describe('ReminderToFocus', () => {
   it("should appear when 'focused' prop is false", async () => {
-    render(<ReminderToFocus focused={false} focusSetter={() => null} />)
+    render(
+      <ReminderToFocus
+        focused={false}
+        focusSetter={() => null}
+        refToFocus={{
+          current: {
+            focus: () => null,
+          },
+        }}
+      />,
+    )
 
     expect(await screen.findByTestId('reminder-to-focus')).toBeInTheDocument()
   })
 
   it("should be hidden when 'focused' prop is true", async () => {
-    render(<ReminderToFocus focused={true} focusSetter={() => null} />)
+    render(
+      <ReminderToFocus
+        focused={true}
+        focusSetter={() => null}
+        refToFocus={{
+          current: {
+            focus: () => null,
+          },
+        }}
+      />,
+    )
 
     expect(screen.queryByTestId('reminder-to-focus')).not.toBeInTheDocument()
   })
@@ -19,7 +39,17 @@ describe('ReminderToFocus', () => {
   it('should hide on click', async () => {
     function Wrapper() {
       const [focused, setFocused] = useState(false)
-      return <ReminderToFocus focused={focused} focusSetter={setFocused} />
+      return (
+        <ReminderToFocus
+          focused={focused}
+          focusSetter={setFocused}
+          refToFocus={{
+            current: {
+              focus: () => null,
+            },
+          }}
+        />
+      )
     }
 
     render(<Wrapper />)
@@ -32,5 +62,32 @@ describe('ReminderToFocus', () => {
     await user.click(reminderToFocus)
 
     expect(reminderToFocus).not.toBeInTheDocument()
+  })
+
+  it('should focus ref passed as a prop', async () => {
+    const mockedRefToFocus = { current: { focus: () => null } }
+    const spyMockedRefToFocus = vi.spyOn(mockedRefToFocus.current, 'focus')
+
+    function Wrapper() {
+      const [focused, setFocused] = useState(false)
+      return (
+        <ReminderToFocus
+          focused={focused}
+          focusSetter={setFocused}
+          refToFocus={mockedRefToFocus}
+        />
+      )
+    }
+
+    render(<Wrapper />)
+    const reminderToFocus = await screen.findByTestId('reminder-to-focus')
+
+    expect(reminderToFocus).toBeInTheDocument()
+
+    const user = userEvent.setup()
+    await user.click(reminderToFocus)
+
+    expect(reminderToFocus).not.toBeInTheDocument()
+    expect(spyMockedRefToFocus).toHaveBeenCalledOnce()
   })
 })
