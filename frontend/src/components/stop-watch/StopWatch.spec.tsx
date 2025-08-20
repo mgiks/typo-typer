fd0ebe8e9392ff76675914dfebd86ebd0789f746
mgiks <lidamil6785@gmail.com>
2025-08-20 19:38:06 +0600

    feat(StopWatch): add functionality to the component

--- frontend/src/components/stop-watch/StopWatch.spec.tsx
+++ frontend/src/components/stop-watch/StopWatch.spec.tsx
@@ -1,1 +1,25 @@
-describe('StopWatch', async () => {})
+import { renderWithProviders } from '../../tests/utils'
+import { act, screen } from '@testing-library/react'
+import StopWatch from './StopWatch'
+
+describe('StopWatch', async () => {
+  it('should update every second', async () => {
+    vi.useFakeTimers()
+
+    renderWithProviders(<StopWatch />, {
+      preloadedState: { isUserTyping: { value: true } },
+    })
+
+    expect(screen.getByText('0')).toBeInTheDocument()
+
+    act(() => vi.advanceTimersByTime(1000))
+
+    expect(screen.getByText('1')).toBeInTheDocument()
+
+    act(() => vi.advanceTimersByTime(1000))
+
+    expect(screen.getByText('2')).toBeInTheDocument()
+
+    vi.useRealTimers()
+  })
+})
--- frontend/src/components/stop-watch/StopWatch.tsx
+++ frontend/src/components/stop-watch/StopWatch.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react'
 import { useAppSelector } from '../../hooks'
 
 function StopWatch() {
-  const timer = <div role='timer' />
+  const [secondsElapsed, setSecondsElapsed] = useState(0)
 
   const isUserTyping = useAppSelector((state) => state.isUserTyping.value)
+
+  useEffect(() => {
+    if (!isUserTyping) return
+
+    const intervalId = setInterval(() => {
+      setSecondsElapsed((s) => s + 1)
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [isUserTyping])
+
+  const timer = <div role='timer'>{secondsElapsed}</div>
 
   return isUserTyping ? timer : null
 }
