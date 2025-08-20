e8135b49d80b3eb3751e169b4e7d281c2929d115
mgiks <lidamil6785@gmail.com>
2025-08-20 20:02:16 +0600

    chore: adjust some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.scss'
+import StopWatch from './components/stop-watch/StopWatch'
 import TypingBox from './components/typing-box/TypingBox'
 
 function App() {
   return (
     <div className='app'>
       <div />
-      <TypingBox />
+      <div className='app__typing-section'>
+        <TypingBox />
+        <StopWatch />
+      </div>
       <div />
     </div>
   )
