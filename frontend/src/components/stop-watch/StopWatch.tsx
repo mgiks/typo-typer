d7d476c4f8a63c4185cf8742ce94c82de43b8971
mgiks <lidamil6785@gmail.com>
2025-08-21 11:26:44 +0600

    fix(StopWatch): prevent time desynchronization

--- frontend/src/components/stop-watch/StopWatch.tsx
+++ frontend/src/components/stop-watch/StopWatch.tsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useAppSelector } from '../../hooks'
 
 function StopWatch() {
-  const [secondsElapsed, setSecondsElapsed] = useState(0)
+  const [milliSecondsElapsed, setMilliSecondsElapsed] = useState(0)
+  const startTime = useRef(0)
 
   const isUserTyping = useAppSelector((state) => state.isUserTyping.value)
 
   useEffect(() => {
     if (!isUserTyping) return
 
+    startTime.current = Date.now()
+
     const intervalId = setInterval(() => {
-      setSecondsElapsed((s) => s + 1)
-    }, 1000)
+      setMilliSecondsElapsed(Date.now() - startTime.current)
+    }, 1)
 
     return () => clearInterval(intervalId)
   }, [isUserTyping])
 
-  const timer = <div role='timer'>{secondsElapsed}</div>
+  const timer = <div role='timer'>{Math.floor(milliSecondsElapsed / 1000)}</div>
 
   return isUserTyping ? timer : null
 }
