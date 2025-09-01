5db821375de47b084d4ca6cb148865837b31f03f
mgiks <lidamil6785@gmail.com>
2025-09-01 11:37:49 +0600

    fix(FocusReminder): fix tests

--- frontend/src/components/typing-box/focus-reminder/FocusReminder.spec.tsx
+++ frontend/src/components/typing-box/focus-reminder/FocusReminder.spec.tsx
@@ -1,15 +1,20 @@
-import { render, screen } from '@testing-library/react'
-import FocusReminder from './FocusReminder'
+import { act, render, screen } from '@testing-library/react'
+import FocusReminder, { FOCUS_REMINDER_TIMEOUT_MS } from './FocusReminder'
 
-describe('FocusReminder', () => {
-  it("should be in the document when 'visible' prop is true", () => {
-    render(<FocusReminder visible={true} />)
+describe('FocusReminder', async () => {
+  it("should be in the document after a timeout when 'visible' prop is true", () => {
+    vi.useFakeTimers()
+    render(<FocusReminder show={true} />)
+
+    act(() => {
+      vi.advanceTimersByTime(FOCUS_REMINDER_TIMEOUT_MS).useRealTimers()
+    })
 
     expect(screen.getByRole('status')).toBeInTheDocument()
   })
 
   it("should not be in the document when 'visible' prop is false", () => {
-    render(<FocusReminder visible={false} />)
+    render(<FocusReminder show={false} />)
 
     expect(screen.queryByRole('status')).not.toBeInTheDocument()
   })
