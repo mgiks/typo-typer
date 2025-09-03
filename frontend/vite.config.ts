afa1ae0ca4ac8eaed80a2b650b106b505f1124ba
mgiks <lidamil6785@gmail.com>
2025-09-03 13:27:46 +0600

    build: switch from '@vitejs/plugin-react-swc' to '@vitejs/plugin-react'

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
