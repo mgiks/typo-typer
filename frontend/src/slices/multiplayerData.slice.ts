4f01649f9179e82c8b4781e19266566d62288e99
mgiks <lidamil6785@gmail.com>
2025-09-06 09:43:27 +0600

    fix: add some missing data in a slice

--- frontend/src/slices/multiplayerData.slice.ts
+++ frontend/src/slices/multiplayerData.slice.ts
@@ -2,10 +2,13 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
 
 export type MultiplayerDataState = {
   name: string
+  averageWpm: number
 }
 
 export const multiplayerDataInitialState: MultiplayerDataState = {
   name: 'placeholder bandit',
+  // World Average WPM (approximately)
+  averageWpm: 40,
 }
 
 export const multiplayerDataSlice = createSlice({
@@ -15,9 +18,12 @@ export const multiplayerDataSlice = createSlice({
     setName: (state, action: PayloadAction<string>) => {
       state.name = action.payload
     },
+    setAverageWpm: (state, action: PayloadAction<number>) => {
+      state.averageWpm = action.payload
+    },
   },
 })
 
-export const { setName } = multiplayerDataSlice.actions
+export const { setName, setAverageWpm } = multiplayerDataSlice.actions
 
 export default multiplayerDataSlice.reducer
