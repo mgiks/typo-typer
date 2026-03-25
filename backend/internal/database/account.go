e8874142dfe60d5bec996d2026d100a393e792b1
mgiks <lidamil6785@gmail.com>
2026-03-25 18:04:15 +0600

    refactor: remove unnecessary parentheses and scan into struct fields instead of the struct itself

--- backend/internal/database/account.go
+++ backend/internal/database/account.go
@@ -9,7 +9,7 @@ import (
 
 func (db *DB) AddAccount(ctx context.Context, username, passhash, salt string) error {
 	if _, err := db.pool.Exec(ctx,
-		"INSERT INTO account (username, passhash, salt) VALUES ($1, $2, $3)",
+		"INSERT INTO account username, passhash, salt VALUES $1, $2, $3",
 		username, passhash, salt); err != nil {
 		return fmt.Errorf("query add account failed: %w", err)
 	}
@@ -17,12 +17,19 @@ func (db *DB) AddAccount(ctx context.Context, username, passhash, salt string) e
 }
 
 func (db *DB) GetAccountByName(ctx context.Context, username string) (*account.Account, error) {
-	row := db.pool.QueryRow(ctx, "SELECT (id, username, email, passhash, salt, wpm) FROM account WHERE username=$1", username)
+	row := db.pool.QueryRow(ctx, "SELECT id, username, email, passhash, salt, wpm FROM account WHERE username=$1", username)
 
-	var a *account.Account
-	if err := row.Scan(a); err != nil {
+	var a account.Account
+	if err := row.Scan(
+		&a.Id,
+		&a.Username,
+		&a.Email,
+		&a.PassHash,
+		&a.Salt,
+		&a.Wpm,
+	); err != nil {
 		return nil, fmt.Errorf("query get account failed: %w", err)
 	}
 
-	return a, nil
+	return &a, nil
 }
