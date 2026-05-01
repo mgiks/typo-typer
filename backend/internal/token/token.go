fd9649f4c079b99a130ae36283dbbadeab39c180
mgiks <lidamil6785@gmail.com>
2026-05-02 03:38:55 +0600

    fix: adapt token service to account repository

--- backend/internal/token/token.go
+++ backend/internal/token/token.go
@@ -9,44 +9,32 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/mgiks/typo-typer/internal/account"
+	"github.com/mgiks/typo-typer/internal/hashing"
+	"github.com/mgiks/typo-typer/internal/store"
 )
-
-type refreshTokenAdder interface {
-	AddRefreshToken(ctx context.Context, tokenHash, salt, accountId string, expiresAt time.Time) error
-}
-
-type accountGetter interface {
-	GetAccountByName(ctx context.Context, username string) (account.Account, error)
-}
-
-type stringHasher interface {
-	HashString(str string) (string, string)
-}
 
 type TokenService struct {
 	privateKey []byte
-	a          refreshTokenAdder
-	h          stringHasher
-	g          accountGetter
+	store      store.Store
+	hasher     hashing.HashingService
 }
 
-func NewService(privateKey string, a refreshTokenAdder, h stringHasher, g accountGetter) (*TokenService, error) {
+func NewService(privateKey string, store store.Store, hasher hashing.HashingService) (*TokenService, error) {
 	key, err := base64.StdEncoding.DecodeString(privateKey)
 	if err != nil {
 		return nil, fmt.Errorf("base64 string encoding failed: %w", err)
 	}
-	return &TokenService{privateKey: key, a: a, h: h, g: g}, nil
+	return &TokenService{privateKey: key, store: store, hasher: hasher}, nil
 }
 
 func (s *TokenService) CreateAccessToken(ctx context.Context, username string) (string, error) {
-	account, err := s.g.GetAccountByName(ctx, username)
+	account, err := s.store.Account().GetAccountByName(ctx, username)
 	if err != nil {
 		return "", fmt.Errorf("failed to find account with such name %s: %w", username, err)
 	}
 
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": account.Id,
+		"sub": account.ID,
 		"exp": jwt.NewNumericDate(time.Now().Add(10 * time.Minute)),
 	})
 	str, err := t.SignedString(s.privateKey)
@@ -81,13 +69,13 @@ func (s *TokenService) CreateRefreshToken(ctx context.Context, username string) 
 	rand.Read(token)
 	tokenStr := base64.RawURLEncoding.EncodeToString(token)
 
-	account, err := s.g.GetAccountByName(ctx, username)
+	account, err := s.store.Account().GetAccountByName(ctx, username)
 	if err != nil {
 		return "", fmt.Errorf("failed to find account with such name %s: %w", username, err)
 	}
 
-	tokenHash, salt := s.h.HashString(tokenStr)
-	if err := s.a.AddRefreshToken(ctx, tokenHash, salt, account.Id, time.Now().AddDate(0, 0, 30)); err != nil {
+	tokenHash, salt := s.hasher.HashString(tokenStr)
+	if err := s.store.RefreshToken().CreateRefreshToken(ctx, tokenHash, salt, account.ID, time.Now().AddDate(0, 0, 30)); err != nil {
 		return "", fmt.Errorf("token database insertion failed: %w", err)
 	}
 
