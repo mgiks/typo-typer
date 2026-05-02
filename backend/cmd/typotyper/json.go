78247796acf96c226fd263eaa8d505ee38c344b7
mgiks <lidamil6785@gmail.com>
2026-05-03 03:41:38 +0600

    fix: write header status in writeJSON function

--- backend/cmd/typotyper/json.go
+++ backend/cmd/typotyper/json.go
@@ -14,5 +14,6 @@ func (app application) jsonResponse(w http.ResponseWriter, status int, data any)
 
 func writeJSON(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
