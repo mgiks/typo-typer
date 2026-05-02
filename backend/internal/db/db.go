d9547bc58311c25d51e22dbe273d07863dfc5271
mgiks <lidamil6785@gmail.com>
2026-05-03 01:56:57 +0600

    feat: add connection options to db creation

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -3,69 +3,25 @@ package db
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func Connect(ctx context.Context) (*pgxpool.Pool, error) {
-	url, err := buildUrl()
+func New(ctx context.Context, url string, maxConns, minIdleConns int32, maxConnIdleTime time.Duration) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(url)
 	if err != nil {
-		return nil, fmt.Errorf("failed to build url: %w", err)
+		return nil, fmt.Errorf("failed to parse db config: %w", err)
 	}
 
-	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
-	defer cancel()
+	config.MaxConns = maxConns
+	config.MinIdleConns = minIdleConns
+	config.MaxConnIdleTime = maxConnIdleTime
 
-	p, err := pgxpool.New(ctx, url)
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create new pool connection to pg db: %w", err)
+		return nil, fmt.Errorf("failed to create new db: %w", err)
 	}
 
-	return p, nil
-}
-
-func findEnvs(keys ...string) (map[string]string, error) {
-	foundEnvs := make(map[string]string)
-	missingEnvs := make([]string, 0)
-
-	for _, key := range keys {
-		val := os.Getenv(key)
-		if len(val) == 0 {
-			missingEnvs = append(missingEnvs, key)
-		} else {
-			foundEnvs[key] = val
-		}
-	}
-
-	if len(missingEnvs) > 0 {
-		return nil, fmt.Errorf("following environment variables are not set: %v", missingEnvs)
-	}
-
-	return foundEnvs, nil
-}
-
-const (
-	pgUserEnv = "POSTGRES_USER"
-	pgPassEnv = "POSTGRES_PASSWORD"
-	pgHostEnv = "POSTGRES_HOST"
-	pgPortEnv = "POSTGRES_PORT"
-	pgDbEnv   = "POSTGRES_DB"
-)
-
-func buildUrl() (string, error) {
-	envs, err := findEnvs(pgUserEnv, pgPassEnv, pgHostEnv, pgPortEnv, pgDbEnv)
-	if err != nil {
-		return "", fmt.Errorf("failed to find pg envs: %w", err)
-	}
-
-	return fmt.Sprintf(
-		"postgresql://%s:%s@%s:%s/%s",
-		envs[pgUserEnv],
-		envs[pgPassEnv],
-		envs[pgHostEnv],
-		envs[pgPortEnv],
-		envs[pgDbEnv],
-	), nil
+	return pool, nil
 }
