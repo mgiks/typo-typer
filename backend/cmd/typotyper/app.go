45dd156004f813bf0b340143acff2d096d5b2aa5
mgiks <lidamil6785@gmail.com>
2026-05-03 01:43:15 +0600

    feat: add application struct

---
+++ backend/cmd/typotyper/app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"time"
+
+	"github.com/mgiks/typo-typer/internal/account"
+	"github.com/mgiks/typo-typer/internal/hashing"
+	"github.com/mgiks/typo-typer/internal/matchmaking"
+	"github.com/mgiks/typo-typer/internal/validation"
+)
+
+type application struct {
+	config         config
+	hashingService hashing.HashingService
+	accountService account.AccountService
+	matchmaker     *matchmaking.MatchMakingService
+	validator      validation.ValidationService
+}
+
+type config struct {
+	port string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	url             string
+	maxConns        int32
+	minIdleConns    int32
+	maxConnIdleTime time.Duration
+}
