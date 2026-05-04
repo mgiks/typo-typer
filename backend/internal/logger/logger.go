737d75fbc776bd46c9e17f62338272d77869c7bd
mgiks <lidamil6785@gmail.com>
2026-05-05 00:37:10 +0600

    feat: add functions to log errors, info and fatal errors in logger service

--- backend/internal/logger/logger.go
+++ backend/internal/logger/logger.go
@@ -1,17 +1,35 @@
 package logger
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+)
 
-type LoggerService struct {
+type LoggerService interface {
+	Info(string, ...any)
+	Error(string, ...any)
+	FatalError(string, ...any)
+}
+
+type loggerService struct {
 	logger slog.Logger
 }
 
 func NewService(logger slog.Logger) LoggerService {
-	return LoggerService{
+	return loggerService{
 		logger: logger,
 	}
 }
 
-func (s LoggerService) Error(msg string, args ...any) {
+func (s loggerService) Info(msg string, args ...any) {
+	s.logger.Info(msg, args...)
+}
+
+func (s loggerService) Error(msg string, args ...any) {
 	s.logger.Error(msg, args...)
+}
+
+func (s loggerService) FatalError(msg string, args ...any) {
+	s.logger.Error(msg, args...)
+	os.Exit(1)
 }
