10b4811a81e93339a1edb9d4545ecc489ceada78
mgiks <lidamil6785@gmail.com>
2026-05-05 23:57:22 +0600

    chore: add context to errors in get random text handlers

--- backend/cmd/api/texts.go
+++ backend/cmd/api/texts.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func (app application) getRandomTextHandler(w http.ResponseWriter, r *http.Request) {
 	text, err := app.textService.GetRandomText(r.Context())
 	if err != nil {
-		app.internalServerError(w, r, err)
+		app.internalServerError(w, r, fmt.Errorf("failed to get random text: %w", err))
 		return
 	}
 
 	if err := app.jsonResponse(w, http.StatusOK, text); err != nil {
-		app.internalServerError(w, r, err)
+		app.internalServerError(w, r, fmt.Errorf("failed to send json response: %w", err))
 	}
 }
