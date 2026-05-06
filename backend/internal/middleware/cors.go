92ffd1dcbfda850603a452bb91510fa520c2d3ae
mgiks <lidamil6785@gmail.com>
2026-05-07 04:04:36 +0600

    fix: return function to handle CORS

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -4,10 +4,3 @@ import (
 	"net/http"
 	"os"
 )
-
-func CORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
-		next.ServeHTTP(w, r)
-	})
-}
